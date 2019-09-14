d1a0ed92e7357059cc62dae145ebb1e9c3853fab
Djan <55193201+bakidjan@users.noreply.github.com>
2019-09-14 12:20:05 +0200

    Update CatalogueRestController.java
    controller uploadPhoto added

--- src/main/java/org/diallo/angular/web/CatalogueRestController.java
+++ src/main/java/org/diallo/angular/web/CatalogueRestController.java
@@ -1,5 +1,6 @@
 package org.diallo.angular.web;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -7,11 +8,15 @@ import org.diallo.angular.dao.ProductRepository;
 import org.diallo.angular.entities.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@CrossOrigin("*")
 public class CatalogueRestController {
 	@Autowired
 	private ProductRepository productRepository;
@@ -21,12 +26,27 @@ public class CatalogueRestController {
 //		this.productRepository = productRepository;
 //	}
 //	
+
+	@GetMapping(path = "/photoProduct/{id}", produces = MediaType.IMAGE_PNG_VALUE)
+	public byte[] getPhoto(@PathVariable("id") Long id) throws Exception {
+		Product p = productRepository.findById(id).get();
+		return Files.readAllBytes(Paths.get(System.getProperty("user.home") + "/ecom/products/" + p.getPhotoName()));
+
+	}
 	
-@GetMapping(path="/photoProduct/{id}", produces= MediaType.IMAGE_PNG_VALUE)
-	public byte[] getPhoto( @PathVariable("id") Long id) throws Exception{
-	Product p = productRepository.findById(id).get();
-	return Files.readAllBytes(Paths.get(System.getProperty("user.home")+"/ecom/products/"+p.getPhotoName()));
-	 
 	
-}
+	//donner le meme nom del'objet MultiPartFile "file" que dans le front angular 
+	//dans la methode UploadPhotoProduct
+	
+	//postmaping parceque ds le front, une methode POST est utilisée
+	@PostMapping( path = "/uploadPhoto/{id}")
+	public void uploadPhoto(MultipartFile file, @PathVariable Long id) throws IOException {
+		Product p = productRepository.findById(id).get();
+		//enregistre la photo avec son nom
+		p.setPhotoName(file.getOriginalFilename());
+		 //p.setPhotoName(id+".png");
+		Files.write(Paths.get(System.getProperty("user.home")+"/ecom/products/"+p.getPhotoName()),file.getBytes());
+		productRepository.save(p);
+	
+	}
 }
