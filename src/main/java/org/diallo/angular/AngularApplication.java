97d68611ff7a114604e56d05bf81570f1a63aacc
Diallo <diallodedjan@gmail.com>
2020-02-22 23:05:14 +0100

    spring boot security for user management

--- src/main/java/org/diallo/angular/AngularApplication.java
+++ src/main/java/org/diallo/angular/AngularApplication.java
@@ -9,13 +9,23 @@ import org.diallo.angular.entities.Category;
 import org.diallo.angular.entities.Product;
 import org.diallo.angular.entities.Role;
 import org.diallo.angular.entities.User;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 import net.bytebuddy.utility.RandomString;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 @SpringBootApplication
 public class AngularApplication implements CommandLineRunner{
@@ -66,4 +76,29 @@ public class AngularApplication implements CommandLineRunner{
 
 	}
 
+}
+@Configuration
+class KeycloackConfig{
+	@Bean
+	KeycloakSpringBootConfigResolver keycloakSpringBootConfigResolver(){
+		return new KeycloakSpringBootConfigResolver();
+	}
+}
+@KeycloakConfiguration
+class KeycloakSpringSecurityConfig extends KeycloakWebSecurityConfigurerAdapter{
+	@Override
+	protected SessionAuthenticationStrategy sessionAuthenticationStrategy(){
+		return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.authenticationProvider(keycloakAuthenticationProvider());
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		super.configure(http);
+		http.authorizeRequests().antMatchers("/products/**").hasAuthority("manager");
+	}
 }
